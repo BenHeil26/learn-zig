e44ac268a8e8398485ad21b33db9d9e80db5d607
Benji Heil <ben.heil.dev@gmail.com>
2026-06-11 17:57:56 -0400

    add some functions and a struct

--- base64_encoder/src/root.zig
+++ base64_encoder/src/root.zig
@@ -1,18 +1,59 @@
 //! By convention, root.zig is the root source file when making a package.
 const std = @import("std");
-const Io = std.Io;
+const testing = std.testing;
 
-/// This is a documentation comment to explain the `printAnotherMessage` function below.
-///
-/// Accepting an `Io.Writer` instance is a handy way to write reusable code.
-pub fn printAnotherMessage(writer: *Io.Writer) Io.Writer.Error!void {
-    try writer.print("Run `zig build test` to run the tests.\n", .{});
+const Base64 = struct {
+    _table: *const [64]u8 = undefined,
+
+    pub fn init() Base64 {
+        const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lower = "abcdefghijklmnopqrstuvwxyz";
+        const num_sym = "0123456789+/";
+        return Base64{
+            ._table = upper ++ lower ++ num_sym,
+        };
+    }
+
+    pub fn _char_at(self: Base64, index: usize) u8 {
+        return self._table[index];
+    }
+};
+
+pub fn _calc_encode_length(input: []const u8) !usize {
+    if (input.len < 3) {
+        return 4;
+    }
+
+    const n_groups: usize = try std.math.divCeil(usize, input.len, 3);
+
+    return n_groups * 4;
 }
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
+pub fn _calc_decode_length(input: []const u8) !usize {
+    if (input.len < 4) {
+        return 3;
+    }
+
+    const n_groups: usize = try std.math.divFloor(usize, input.len, 4);
+    var multiple_groups = n_groups * 3;
+
+    for (1..input.len + 1) |i| {
+        if (input[input.len - i] == '=') {
+            multiple_groups -= 1;
+        } else break;
+    }
+    return multiple_groups;
 }
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
+test "_char_at 28" {
+    const base64 = Base64.init();
+    try testing.expect(base64._char_at(28) == 'c');
+}
+
+test "_calc_encode_length" {
+    const test_cases = .{ .{ "hi", 4 }, .{ "longer", 8 } };
+    inline for (test_cases) |test_case| {
+        const result = try _calc_encode_length(test_case.@"0");
+        try testing.expectEqual(test_case.@"1", result);
+    }
 }
